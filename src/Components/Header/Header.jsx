b21343d8cfe06ae9003baadf77b27f733233b308
REZOAN93 <rezoanshawonweb@gmail.com>
2023-09-14 19:31:20 +0600

    Create the fakedata and setup the tailwind

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className=" text-3xl font-bold text-center my-5">Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
